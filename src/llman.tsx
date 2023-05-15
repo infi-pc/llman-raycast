147cdf3daf7ba5eec4d8f772b3fb86dd3735d298
Michael Musil <infi.pc@gmail.com>
2023-05-15 12:16:25 +0200

    improve

--- src/llman.tsx
+++ src/llman.tsx
@@ -1,7 +1,20 @@
-import { getSelectedText, Clipboard, showToast, Toast, AI, List, ActionPanel, Action } from "@raycast/api";
+import { getSelectedText, Clipboard, showToast, Toast, AI, List, ActionPanel, Action, Detail } from "@raycast/api";
 import { useEffect, useState } from "react";
 
+function useDebounce(value: string, timeout: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setDebouncedValue(value), timeout);
+
+    return () => clearTimeout(timeoutId);
+  }, [value, timeout]);
+
+  return debouncedValue;
+}
+
 export default function Command() {
+  const [searchText, setSearchText] = useState("");
   const [actions, setActions] = useState<
     | {
         status: "loading";
@@ -13,6 +26,10 @@ export default function Command() {
   >({
     status: "loading",
   });
+
+  const debouncedSearchText = useDebounce(searchText, 500);
+
+  const [results, setResults] = useState<string | null>(null);
 
   const [processingAction, setProcessingAction] = useState(false);
   useEffect(() => {
@@ -30,28 +47,68 @@ export default function Command() {
         return;
       }
 
+      const noPrompts = debouncedSearchText ? 1 : 10;
       const res = await AI.ask(
-        `I am using GPT to transform text. I will give you text, and you will give me 5 prompts for transformations that can be done with the text. Text can be code, raw data, written text or any other data in text format. Prompts can be for example: "Convert CSS to CSS-in-JS format" if you recognise the text is CSS, "Convert json to yaml" in case the text is JSON, "Make the text more formal" for written texts, "Fix grammar" in case you see some grammar issues, "Fix syntax errors" in case it is a code and you see it has syntax errors etc. 
+        `I am using GPT to transform text. I will give you text, and you will give me ${noPrompts} prompts for transformations that can be done with the text. 
+        Text can be code, raw data, written text or any other data in text format. 
+        Prompts can be for example: 
+        "Convert CSS to CSS-in-JS format" if you recognise the text is CSS, 
+        "Convert json to yaml" in case the text is JSON, 
+        "Make the text more formal" for written texts, 
+        "Fix grammar" in case you see some grammar issues, 
+        "Fix syntax errors" in case it is a code and you see it has syntax errors etc. 
 
-      Now here is the text and you give me the 5 prompts, one per line: \`${selectedText}\``,
+        ${debouncedSearchText ? `I search for prompts containing the text "${debouncedSearchText}"` : ""}
+
+        Don't say anything else than the prompts.
+
+      Now here is the text and you give me the ${noPrompts} prompts, one per line: \`${selectedText}\``,
         {
           model: "gpt-3.5-turbo",
         }
       );
 
-      setActions({
-        status: "success",
-        data: res.trim().split("\n"),
-      });
+      if (debouncedSearchText === searchText) {
+        setActions({
+          status: "success",
+          data: res.trim().split("\n"),
+        });
+      }
+
       //   const transformedText = await AI.ask(`Convert named function to arrow function: \`${selectedText}\``);
 
       //   await Clipboard.paste(transformedText);
     }
     run();
-  }, []);
+  }, [debouncedSearchText]);
+
+  if (results) {
+    const codeResult = results.match(/```(.*)```/s);
+    const codeResultString = codeResult ? codeResult[1] : null;
+    return (
+      <Detail
+        markdown={results}
+        actions={
+          codeResultString ? (
+            <ActionPanel>
+              <Action
+                title="Paste the Code"
+                onAction={async () => {
+                  Clipboard.paste(codeResultString);
+                }}
+              />
+            </ActionPanel>
+          ) : undefined
+        }
+      />
+    );
+  }
 
   return (
-    <List isLoading={actions.status === "loading" || processingAction}>
+    <List
+      isLoading={actions.status === "loading" || processingAction}
+      onSearchTextChange={(text) => setSearchText(text)}
+    >
       {actions.status === "success"
         ? actions.data.map((action, index) => (
             <List.Item
@@ -64,10 +121,18 @@ export default function Command() {
                     title="Apply"
                     onAction={async () => {
                       setProcessingAction(true);
-                      const res = await AI.ask(`${action}: \`${await getSelectedText()}\``, {
-                        model: "gpt-3.5-turbo",
-                      });
-                      Clipboard.paste(res);
+                      const res = await AI.ask(
+                        `I will give you text, you will apply this action on it: \`${action}\`. 
+                        Try to include only one code snippet and put it into markdown code block. 
+                        The code snippet can be used to directly replace the original text, so unless it is explicitely said, try too keep the original data and change only what is said in the "action". 
+
+                        Text:\`${await getSelectedText()}\``,
+                        {
+                          model: "gpt-3.5-turbo",
+                        }
+                      );
+                      setResults(res);
+                      // Clipboard.paste(res);
                       setProcessingAction(false);
                     }}
                   />
